8523ffb1b303ca8ac28982ac897a2e29a60f15d7
pwright117 <50474799+pwright117@users.noreply.github.com>
2019-06-16 15:41:50 -0700

    Update Assignment 9.js

--- Assignment 9.js
+++ Assignment 9.js
@@ -7,44 +7,80 @@ class Playlist {
   }
 }
 
-class Song extends Playlist {
-  constructor(name, duration, artist, rating) {
-    super(name, duration, artist, rating);
+  class Song extends Playlist {
+    constructor(name, duration, artist, rating) {
+      super(name, duration, artist, rating);
+    }
+    getName() {
+      document.getElementById("mediaName").innerHTML = ("Name: " + this.name + "<br> Artist: " + this.artist + "<br> Duration: " + this.duration);
+    }
   }
-  getName() {
-    console.log("\n Name: " + this.name + "\n Artist: " + this.artist + "\n Duration: " + this.duration);
+
+  class Podcast extends Playlist {
+    constructor(name, duration, artist, rating) {
+      super(name, duration, artist, rating);
+
+    }
+    getName() {
+      document.getElementById("mediaName").innerHTML = ("Name: " + this.name + "<br> Duration: " + this.duration);
+    }
   }
-}
 
-class Podcast extends Playlist {
-  constructor(name, duration, artist, rating) {
-    super(name, duration, artist, rating);
-
+  class Movie extends Playlist {
+    constructor(name, duration, artist, rating) {
+      super(name, duration, artist, rating);
+    }
+    getName() {
+      document.getElementById("mediaName").innerHTML = ("Name: " + this.name + "<br> Rating: " + this.rating + "<br> Duration: " + this.duration);
+    }
   }
-  getName() {
-    console.log("\n Name: " + this.name + "\n Duration: " + this.duration);
+  let a = new Song("Love On Top","3:20","Beyonce")
+  let b = new Podcast("Talk The Talk", "10:00")
+  let c = new Movie("Avengers:End Game", "FOREVER", "", "5 Stars")
+  let d = new Podcast("Podcast: The Podcast", "10:00:00")
+  let e = new Movie("Inception", "3:50", "", "4 stars")
+
+  var playlist = [a, b, c, d, e]
+
+  playlist.push(new Movie ("Sexy", "2:00", "", "0 Stars"))
+  //add to playlist
+
+  let holder=0
+
+  function play() {
+    if (holder == -1){
+      holder = 0
+      playlist[holder].getName()
+    } else {
+    playlist[holder].getName()
+    }
   }
-}
 
-class Movie extends Playlist {
-  constructor(name, duration, artist, rating) {
-    super(name, duration, artist, rating);
+  function pause() {
+    document.getElementById("mediaName").innerHTML = ("")
   }
-  getName() {
-    console.log("\n Name: " + this.name + "\n Rating: " + this.rating + "\n Duration: " + this.duration);
+
+  function next() {
+    if (holder == -1) {
+      holder = 0
+      playlist[holder].getName()
+    } else {
+      holder++
+      if (holder>=playlist.length) {
+        holder=playlist.length
+        document.getElementById("mediaName").innerHTML = ("")
+      } else {
+        playlist[holder].getName()
+      }
+    }
+    }
+
+  function previous(){
+    holder--
+    if (holder<0) {
+      holder=-1
+      document.getElementById("mediaName").innerHTML = ("")
+    } else {
+    playlist[holder].getName();
+    }
   }
-}
-let a = new Song("Love On Top","3:20","Beyonce")
-let b = new Podcast("Talk The Talk", "10:00")
-let c = new Movie("Avengers:End Game", "FOREVER", "5 Stars")
-let d = new Podcast("Podcast: The Podcast", "10:00:00")
-let e = new Movie("Inception", "3:50", "4 Stars")
-
-var playlist = [a, b, c, d, e]
-
-playlist[0].getName()
-
-function next(){
-  i+=0
-  playlist[i].getName()
-}
