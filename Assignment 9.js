58f18129738c397be06f86bc4b04c8ca4bf31c2b
pwright117 <50474799+pwright117@users.noreply.github.com>
2019-06-16 15:18:33 -0700

    new edit

--- Assignment 9.js
+++ Assignment 9.js
@@ -1,4 +1,4 @@
-class Media {
+class Playlist {
   constructor(name, duration, artist, rating) {
     this.name = name;
     this.duration = duration;
@@ -7,34 +7,44 @@ class Media {
   }
 }
 
-class Music extends Media {
+class Song extends Playlist {
   constructor(name, duration, artist, rating) {
-    super(name)
-    super(artist)
-    super(duration)
+    super(name, duration, artist, rating);
   }
   getName() {
-    console.log(this.name + " " + this.artist + " " + this.duration);
+    console.log("\n Name: " + this.name + "\n Artist: " + this.artist + "\n Duration: " + this.duration);
   }
 }
 
-class Podcast extends Media {
+class Podcast extends Playlist {
   constructor(name, duration, artist, rating) {
-    super(name);
+    super(name, duration, artist, rating);
 
   }
   getName() {
-    console.log(this.name + " " + this.duration);
+    console.log("\n Name: " + this.name + "\n Duration: " + this.duration);
   }
 }
 
-class Movie extends Media {
+class Movie extends Playlist {
   constructor(name, duration, artist, rating) {
-    super(name);
-    super(rating);
-    super(duration);
+    super(name, duration, artist, rating);
   }
   getName() {
-    console.log(this.name + " " + this.rating + " " + this.duration);
+    console.log("\n Name: " + this.name + "\n Rating: " + this.rating + "\n Duration: " + this.duration);
   }
+}
+let a = new Song("Love On Top","3:20","Beyonce")
+let b = new Podcast("Talk The Talk", "10:00")
+let c = new Movie("Avengers:End Game", "FOREVER", "5 Stars")
+let d = new Podcast("Podcast: The Podcast", "10:00:00")
+let e = new Movie("Inception", "3:50", "4 Stars")
+
+var playlist = [a, b, c, d, e]
+
+playlist[0].getName()
+
+function next(){
+  i+=0
+  playlist[i].getName()
 }
